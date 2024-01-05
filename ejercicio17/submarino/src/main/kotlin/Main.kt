08016609535a0ce6e54c52019a8b8d9543ab4939
JesusCoboArrogante <jesusoscar.cobo@alumno.iesluisvives.org>
2024-01-05 17:44:31 +0100

    first commit

--- ejercicio17/submarino/src/main/kotlin/Main.kt
+++ ejercicio17/submarino/src/main/kotlin/Main.kt
@@ -1,13 +1,57 @@
 fun main(args: Array<String>) {
-    var oceano = IntArray(20)
-    val numero = 0
-    jugador (numero)
+
+    var oceanoA = IntArray(10)
+    var oceanoB = IntArray(10)
+    var contador = 0
+
+
+    do {
+        println("que empiece el juego")
+        var numero = (0..9).random()
+        jugador(numero, oceanoA, contador)
+        contador = jugador(numero, oceanoA, contador)
+
+    }while (contador<4)
+
+
 
 }
 
-fun jugador(numero: Int) {
-    for (i in 1..4){
-        println("donde quieres poner el barco")
-        numero = readln().toIntOrNull()?:0
+fun jugador(numero: Int , oceanoA: IntArray, contador: Int): Int{
+
+
+
+        if (oceanoA[numero] == 0 ){
+            oceanoA[numero] = 4
+            contador+1
+
+            return contador
+
+        }
+
+
+
+
+    return contador
+
+
+
+
+
+
+}
+
+
+
+fun escainet (oceanoB: IntArray){
+
+    for (i in oceanoB.indices){
+        var aleatorio = (0..19).random()
+        if (oceanoB[i] != 8){
+            oceanoB[aleatorio] = 8
+        }else{
+
+        }
+
     }
 }
